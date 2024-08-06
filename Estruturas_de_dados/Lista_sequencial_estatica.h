4e30aa418585b5703c0b77005d5f0f859e64de38
Lucca Nobre Mutran Dalla Bernardina <159442656+Lucca0404@users.noreply.github.com>
2024-08-06 16:24:15 -0300

    Create Lista_sequencial_estatica.h

---
+++ Estruturas_de_dados/Lista_sequencial_estatica.h
@@ -0,0 +1,35 @@
+#define MAX 100
+
+struct aluno{
+    int matricula;
+    char nome[30];
+    float n1,n2,n3;
+};
+
+Typedef struct lista Lista;
+
+Lista* Lista_criar();
+
+void Lista_apagar(Lista* li);
+
+int Lista_busca_pos(Lista* li, int pos, struct aluno *al);
+
+int Lista_busca_mat(Lista* li, int mat, struct aluno *al);
+
+int Lista_insere_inicio(Lista* li, struct aluno al);
+
+int Lista_insere_final(Lista* li, struct aluno al);
+
+int Lista_insere_ordenada(Lista* li, struct aluno al);
+
+int Lista_remove(Lista* li, int mat);
+
+int Lista_remove_inicio(Lista* li);
+
+int Lista_remove_final(Lista* li);
+
+int Lista_tamanho(Lista* li);
+
+int Lista_cheia(Lista* li);
+
+int Lista_vazia(Lista* li);
