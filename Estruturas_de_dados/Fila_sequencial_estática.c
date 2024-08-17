b52b96d010c8075c2161844d4b2f0488dde86ee5
Lucca Nobre Mutran Dalla Bernardina <159442656+Lucca0404@users.noreply.github.com>
2024-08-17 14:31:46 -0300

    Create Fila_sequencial_estática.c

---
+++ Estruturas_de_dados/Fila_sequencial_estática.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "Filas.h"
+
+struct fila{
+    int inicio;
+    int final;
+    int qtd;
+    struct aluno dados[MAX];
+};
+
+Fila* Fila_criar(){
+    Fila* fi = (Fila*) malloc(sizeof(Fila));
+    if(fi != NULL){
+        fi->inicio = 0;
+        fi->final = 0;
+        fi->qtd = 0;
+    }
+    return fi;
+}
+
+void Fila_apagar(Fila* fi){
+    free(fi);
+}
+
+int Fila_cheia(Fila* fi){
+    if(fi == NULL){
+        return -1;
+    }
+    (fi->qtd == MAX);
+}
+
+int Fila_vazia(Fila* fi){
+    if(fi == NULL){
+        return -1;
+    }
+    return (fi->qtd == 0);
+}
+
+int Fila_tamanho(Fila* fi){
+    if(fi == NULL){
+        return -1;
+    }
+    return fi->qtd;
+}
+
+int Fila_inserir(Fila* fi, struct aluno al){
+    if(fi == NULL || Fila_cheia(fi)){
+        return 0;
+    }
+    fi->dados[fi->final] = al;
+    fi->final = (fi->final+1) % MAX;
+    fi->qtd++;
+    return 1;
+}
+
+int Fila_remover(Fila* fi){
+    if(fi == NULL || Fila_vazia(fi)){
+        return 0;
+    }
+    fi->inicio = (fi->inicio+1) % MAX;
+    fi->qtd--;
+    return 1;
+}
+
+int Fila_consultar(Fila* fi, struct aluno *al){
+    if(fi == NULL || Fila_vazia(fi)){
+        return 0;
+    }
+    *al = fi->dados[fi->incio];
+    return 1;
+}
