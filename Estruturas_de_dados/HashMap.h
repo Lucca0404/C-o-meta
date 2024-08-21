c18d93b09bf813c1f6a23446fe408ee0e6a8f575
Lucca Nobre Mutran Dalla Bernardina <159442656+Lucca0404@users.noreply.github.com>
2024-08-21 10:40:37 -0300

    Create HashMap.h

---
+++ Estruturas_de_dados/HashMap.h
@@ -0,0 +1,16 @@
+struct aluno{
+    int mat;
+    char nome[30];
+}
+
+typedef struct hash Hash;
+
+Hash* cria_hash(int tamanho);
+
+void libera_hash(Hash* ha);
+
+int valor_string(char *str);
+
+int insere_sem_colisao_hash(Hash* ha, struct aluno al);
+
+int busca_sem_colisao_hash(Hash* ha, int mat, struct aluno *al);
